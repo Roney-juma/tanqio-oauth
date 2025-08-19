23a42fa43d44923f53ebac6025d1d84aba0afe02
Roney Juma <ochirone10@gmail.com>
2025-08-19 09:13:04 +0300

    Update

--- oauth/settings.py
+++ oauth/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',  # disable default static handling to let WhiteNoise manage it
+    'whitenoise.runserver_nostatic',
     'oauth2_provider',
     'social_django',
     'tanqio',
@@ -269,7 +269,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / 'tanqio' / 'images']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
